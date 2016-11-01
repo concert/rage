12e366eec77bab72c664bbc984a95b01922b5f18
David Honour <david@foolswood.co.uk>
2016-11-01 19:56:28 +0000

    Implement the ports stuff

---
+++ src/ports.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include "ports.h"
+
+rage_PortDescription * rage_port_description_copy(rage_PortDescription pd) {
+    rage_PortDescription * npd = malloc(sizeof(rage_PortDescription));
+    npd->is_input = pd.is_input;
+    npd->type = pd.type;
+    npd->next = pd.next;
+    switch (pd.type) {
+        case (RAGE_PORT_STREAM):
+            npd->stream_def = pd.stream_def;
+            break;
+        case (RAGE_PORT_EVENT):
+            npd->event_def = pd.event_def;
+            break;
+    }
+    return npd;
+}
+
+void rage_port_description_free(rage_PortDescription * pdp) {
+    free(pdp);
+}
---
+++ test/test_ports.c
@@ -0,0 +1,17 @@
+#include "ports.h"
+#include <stdlib.h>
+
+int main() {
+    rage_PortDescription pd = {
+        .is_input = true, .type = RAGE_PORT_STREAM,
+        .stream_def = RAGE_STREAM_AUDIO, .next=NULL};
+    rage_PortDescription * npd = rage_port_description_copy(pd);
+    if (!npd->is_input)
+        return 1;
+    if (npd->type != RAGE_PORT_STREAM)
+        return 2;
+    if (npd->next != NULL)
+        return 3;
+    if (npd->stream_def != RAGE_STREAM_AUDIO)
+        return 4;
+}
