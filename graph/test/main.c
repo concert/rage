e75f92724c83c7dd113af39c7bed89e8f445f450
David Honour <david@foolswood.co.uk>
2016-11-15 21:21:09 +0000

    Loop over tests to run...
    diagnostics stink, but it's a start.

--- graph/test/main.c
+++ graph/test/main.c
@@ -1,15 +1,19 @@
 #include "test_loader.c"
 #include "test_engine.c"
 
+typedef rage_Error (*rage_TestFunc)();
+
 int main() {
-    rage_Error e = test();
-    if (RAGE_FAILED(e)) {
-        printf("Failed: %s\n", RAGE_FAILURE_VALUE(e));
-        return 1;
-    }
-    e = test_engine();
-    if (RAGE_FAILED(e)) {
-        printf("Failed: %s\n", RAGE_FAILURE_VALUE(e));
-        return 1;
+    unsigned const len = 2;
+    rage_TestFunc tests[] = {
+        test,
+        test_engine
+    };
+    for (unsigned i=0; i < len; i++) {
+        rage_Error e = tests[i]();
+        if (RAGE_FAILED(e)) {
+            printf("Failed: %s\n", RAGE_FAILURE_VALUE(e));
+            return 1;
+        }
     }
 }
