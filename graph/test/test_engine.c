6fca6367ff3b0139524069fec4fb7df08abe2f72
David Honour <david@foolswood.co.uk>
2016-11-21 21:54:03 +0000

    Simplify test slightly

--- graph/test/test_engine.c
+++ graph/test/test_engine.c
@@ -2,8 +2,7 @@
 
 rage_Error test_engine() {
     rage_NewEngine ne = rage_engine_new();
-    rage_Engine * e;
-    RAGE_EXTRACT_VALUE(rage_Error, ne, e)
+    RAGE_EXTRACT_VALUE(rage_Error, ne, rage_Engine * e)
     rage_engine_free(e);
     RAGE_OK
 }
