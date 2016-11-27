2600df8b4d595fe5b858037630bc0ea73206813f
David Honour <david@foolswood.co.uk>
2016-11-27 08:42:04 +0000

    Make sure ports new/free cycle doesn't die

--- types/test/test_ports.c
+++ types/test/test_ports.c
@@ -2,5 +2,23 @@
 #include <stdlib.h>
 
 int main() {
-    // FIXME: reinstate testing of this
+    rage_AtomDef const unconstrained_float = {
+        .type = RAGE_ATOM_FLOAT, .name = "float", .constraints = {}
+    };
+    rage_FieldDef fields[] = {
+        {.name = "f_field", .type = &unconstrained_float}
+    };
+    rage_TupleDef td = {
+        .name = "Ports Test",
+        .description = "Testing's great",
+        .len = 1,
+        .items = fields
+    };
+    rage_ProcessRequirements pd;
+    pd.controls.items = &td;
+    pd.controls.len = 1;
+    pd.inputs.len = 2;
+    pd.outputs.len = 3;
+    rage_Ports ports = rage_ports_new(&pd);
+    rage_ports_free(ports);
 }
