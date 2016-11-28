7cf0b4feed2e76efe9448040640a4cdd30cd392a
David Honour <david@foolswood.co.uk>
2016-11-28 07:33:21 +0000

    Tidy up TimeSeries creation

--- types/src/time_series.c
+++ types/src/time_series.c
@@ -2,17 +2,12 @@
 #include "time_series.h"
 
 rage_TimeSeries rage_time_series_new(rage_TupleDef const * const item_def) {
-    // FIXME: really crappy and hard coded
-    rage_TimeSeries ts;
-    ts.len = 1;
     rage_TimePoint * tp = malloc(sizeof(rage_TimePoint));
-    tp->time.second = 0;
-    tp->time.fraction = 0;
+    tp->time = (rage_Time) {.second = 0};
     tp->value = rage_tuple_generate(item_def);
     tp->value[0].f = 0;
     tp->mode = RAGE_INTERPOLATION_CONST;
-    ts.items = tp;
-    return ts;
+    return (rage_TimeSeries) {.len = 1, .items = tp};
 }
 
 void rage_time_series_free(rage_TimeSeries ts) {
