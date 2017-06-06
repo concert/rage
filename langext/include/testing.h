61d09c52ba5b33288d66266e0288551820ba6533
David Honour <david@foolswood.co.uk>
2017-06-06 08:24:13 +0100

    Move test framework over to TAP

--- langext/include/testing.h
+++ langext/include/testing.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include "error.h"
 
 typedef rage_Error (*rage_TestFunc)();
@@ -9,12 +11,21 @@ typedef rage_Error (*rage_TestFunc)();
     int main() { \
         rage_TestFunc const tests[] = {__VA_ARGS__}; \
         unsigned const n_tests = sizeof(tests) / sizeof(rage_TestFunc); \
+        char * test_names = strdup(#__VA_ARGS__); \
+        const char * const delimiters = ", "; \
+        char * saveptr; \
+        char * test_name = strtok_r(test_names, delimiters, &saveptr); \
+        printf("1..%u\n", n_tests); \
         for (unsigned i=0; i < n_tests; i++) { \
-            printf("Running %u " #__VA_ARGS__ "\n", i); \
             rage_Error e = tests[i](); \
             if (RAGE_FAILED(e)) { \
-                printf("Failed: %s\n", RAGE_FAILURE_VALUE(e)); \
+                printf("not ok %u %s\n", i, test_name); \
+                printf("Bail Out! %s\n", RAGE_FAILURE_VALUE(e)); \
                 return 1; \
+            } else { \
+                printf("ok %u %s\n", i, test_name); \
             } \
+            test_name = strtok_r(NULL, delimiters, &saveptr); \
         } \
+        free(test_names); \
     }
