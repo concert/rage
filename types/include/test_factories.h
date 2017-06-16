d8c7af83e4e5db2a393ba8403df8c4d0e8b87c86
David Honour <david@foolswood.co.uk>
2017-06-16 07:40:46 +0100

    Add a test for interpolation test factory...
    it fails :(, let's test the subcomponents.

--- types/include/test_factories.h
+++ types/include/test_factories.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "time_series.h"
 
 /*
  * Makes a single time point time series based on the given discription.
---
+++ types/test/test_test_factories.c
@@ -0,0 +1,60 @@
+#include <string.h>
+#include "atoms.h"
+#include "test_factories.h"
+#include "error.h"
+#include "testing.h"
+
+rage_Error test_time_series_new() {
+    const rage_AtomDef int_type = {
+        .type = RAGE_ATOM_INT,
+        .name = "inty",
+        .constraints = {.i = (rage_IntConstraints) {
+             .min = RAGE_JUST(12), .max = RAGE_NOTHING
+        }}
+    };
+    const rage_AtomDef string_type = {
+        .type = RAGE_ATOM_STRING,
+        .name = "stringy",
+        .constraints = {.s = (rage_StringConstraints) RAGE_NOTHING}
+    };
+    const rage_FieldDef defs[] = {
+        {.name = "wheat", .type = &int_type},
+        {.name = "barley", .type = &string_type}
+    };
+    const rage_TupleDef td = {
+        .name = "basil",
+        .description = "herb",
+        .default_value = NULL,
+        .items = defs,
+        .len = 2
+    };
+    rage_TimeSeries ts = rage_time_series_new(&td);
+    char * err = NULL;
+    if (ts.len != 1) {
+        err = "timeseries is not 1 point long";
+    }
+    if ((err == NULL) && ts.items == NULL) {
+        err = "timeseries items is NULL";
+    }
+    rage_TimePoint * tp = ts.items;
+    if ((err == NULL) && (tp->time.second || tp->time.fraction)) {
+        err = "timeseries does not start at t=0";
+    }
+    if ((err == NULL) && tp->mode != RAGE_INTERPOLATION_CONST) {
+        err = "final time point has non-constant interpolation";
+    }
+    if ((err == NULL) && tp->value[0].i != 12) {
+        err = "integer has taken unexpected value";
+    }
+    if ((err == NULL) && !strcmp(tp->value[1].s, "")) {
+        err = "wasn't an empty string";
+    }
+    rage_time_series_free(ts);
+    if (err == NULL) {
+        RAGE_OK
+    } else {
+        RAGE_ERROR(err)
+    }
+}
+
+TEST_MAIN(test_time_series_new)
