fe7d578da28170b13bf79ae4a4636baac3fa1055
David Honour <david@foolswood.co.uk>
2017-07-06 07:10:44 +0100

    Make ports test slightly better (and within framework)

--- types/test/test_ports.c
+++ types/test/test_ports.c
@@ -1,8 +1,10 @@
+#include "testing.h"
+#include "error.h"
 #include "ports.h"
 #include <stdlib.h>
 
-int main() {
-    // FIXME: This isn't really a test.
+rage_Error test_ports() {
+    // TODO: come up with something more than execution for this?
     rage_AtomDef const unconstrained_float = {
         .type = RAGE_ATOM_FLOAT, .name = "float", .constraints = {}
     };
@@ -21,5 +23,12 @@ int main() {
     pd.inputs.len = 2;
     pd.outputs.len = 3;
     rage_Ports ports = rage_ports_new(&pd);
+    rage_Error rval = RAGE_OK;
+    if (ports.inputs == NULL) {
+        rval = RAGE_ERROR("Input array points to null");
+    }
     rage_ports_free(ports);
+    return rval;
 }
+
+TEST_MAIN(test_ports)
