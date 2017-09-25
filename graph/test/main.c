0458640183f3ca1c63b9c8decda5518a16e0af08
David Honour <david@concertdaw.co.uk>
2017-09-25 11:22:38 +0100

    Add very basic graph stop/start test

--- graph/test/main.c
+++ graph/test/main.c
@@ -3,5 +3,6 @@
 #include "test_loader.c"
 #include "test_jack_bindings.c"
 #include "test_srt.c"
+#include "test_graph.c"
 
-TEST_MAIN(test_loader, test_jack_bindings, test_srt, test_srt_fake_elem)
+TEST_MAIN(test_loader, test_jack_bindings, test_srt, test_srt_fake_elem, test_graph)
