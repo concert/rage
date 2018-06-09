1f2a54e4745f9cd75b963616adf8d722c40f3561
David Honour <david@concertdaw.co.uk>
2018-06-09 07:16:11 +0100

    Add my current hypothesis as to why it doesn't work

--- stoat_check.sh
+++ stoat_check.sh
@@ -7,6 +7,8 @@ fi
 
 #ln -s /usr/lib/libstdc++.so.6 /usr/lib/libstdc++.so
 
+# I think the issue is stemming from us being a C project, and stoat being C++
+
 git clone https://github.com/fundamental/stoat && pushd stoat
 mkdir build && cd build
 cmake .. && make && make test
