903c37d9ea5c9815d3b88d90ba4ff8bdb16b657e
David Honour <david@concertdaw.co.uk>
2018-06-09 07:01:56 +0100

    Try a different approach to getting stoat deps

--- stoat_check.sh
+++ stoat_check.sh
@@ -5,13 +5,12 @@ if [[ $CC != clang* ]]; then
     exit
 fi
 
-sudo apt-get install ruby
-sudo ln -s /usr/lib/libstdc++.so.6 /usr/lib/libstdc++.so
+ln -s /usr/lib/libstdc++.so.6 /usr/lib/libstdc++.so
 
 git clone https://github.com/fundamental/stoat && pushd stoat
 mkdir build && cd build
 cmake .. && make && make test
-sudo make install
+make install
 popd
 
 export CC=stoat-compile
