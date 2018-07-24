1e522aeca8479d28b79aba8e985e21d6af931cd3
David Honour <david@concertdaw.co.uk>
2018-07-24 10:20:23 +0100

    Introduce time estimation mechanism...
    this gives us something approaching "sound card" time to use as our
    timestamps for the relay.

--- graph/include/jack_bindings.h
+++ graph/include/jack_bindings.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "error.h"
 #include "binding_interface.h"
+#include "chronology.h"
 
 typedef struct rage_BackendState rage_JackBackend;
 typedef RAGE_OR_ERROR(rage_JackBackend *) rage_NewJackBackend;
@@ -9,3 +10,5 @@ rage_NewJackBackend rage_jack_backend_new(rage_BackendConfig const conf);
 void rage_jack_backend_free(rage_JackBackend * jbe);
 rage_Error rage_jack_backend_activate(rage_JackBackend * jbe);
 rage_Error rage_jack_backend_deactivate(rage_JackBackend * jbe);
+
+rage_Time rage_jack_backend_nowish(rage_JackBackend * jbe);
