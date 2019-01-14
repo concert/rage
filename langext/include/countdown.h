92f2e37ae1a5d657fb1b14860cb9746d8cd5d221
grahamegee <grahamegee@gmail.com>
2019-01-14 16:50:54 +0000

    Generalise countdown

--- langext/include/countdown.h
+++ langext/include/countdown.h
@@ -1,6 +1,8 @@
 #pragma once
 #include "error.h"
 
+typedef void (*rage_CountdownAction)(void * args);
+
 /*
  * In some senses the opposite of a semaphore, it triggers when the count
  * reaches 0 (as opposed to blocking until it is positive).
@@ -11,23 +13,20 @@ typedef struct rage_Countdown rage_Countdown;
  * New countdown (on the heap), initial_value is the starting value of the
  * counter. New instances are in the "blocking" state.
  */
-rage_Countdown * rage_countdown_new(int initial_value);
+rage_Countdown * rage_countdown_new(
+    int initial_value, rage_CountdownAction action, void * args);
 /*
  * Free rage_Countdown
  */
 void rage_countdown_free(rage_Countdown * c);
 
 /*
- * Wait for the countdown to hit 0 for up to the requested time in
- * milliseconds.
- */
-rage_Error rage_countdown_timed_wait(rage_Countdown * c, unsigned millis);
-/*
  * Add a value to the countdown (note the argument is signed).
  * This is a realtime safe operation.
  */
 int rage_countdown_add(rage_Countdown * c, int delta);
+
 /*
  * Reset the countdown and release waiter, returns previous value.
  */
-int rage_countdown_unblock_wait(rage_Countdown * c);
+int rage_countdown_force_action(rage_Countdown * c);
--- langext/src/countdown.c
+++ langext/src/countdown.c
@@ -1,58 +1,42 @@
 #include "countdown.h"
 #include <stdatomic.h>
-#include <semaphore.h>
 #include <stdlib.h>
-#include <time.h>
+
 
 struct rage_Countdown {
     _Atomic int counter;
-    sem_t sig;
+    rage_CountdownAction action;
+    void * args;
 };
 
 _Static_assert(
     ATOMIC_INT_LOCK_FREE, "Atomic int not lock free on this platform");
 
-rage_Countdown * rage_countdown_new(int initial_value) {
+rage_Countdown * rage_countdown_new(
+        int initial_value, rage_CountdownAction action, void * args) {
     rage_Countdown * c = malloc(sizeof(rage_Countdown));
     atomic_init(&c->counter, initial_value);
-    sem_init(&c->sig, 0, 0);
+    c->action = action;
+    c->args = args;
     return c;
 }
 
 void rage_countdown_free(rage_Countdown * c) {
-    sem_destroy(&c->sig);
     free(c);
-}
-
-rage_Error rage_countdown_timed_wait(rage_Countdown * c, unsigned millis) {
-    struct timespec ts;
-    if (clock_gettime(CLOCK_REALTIME, &ts) == -1) {
-        return RAGE_ERROR("Unable to get current time");
-    }
-    ts.tv_sec += millis / 1000;
-    ts.tv_nsec += (millis % 1000) * 1000000;
-    if (ts.tv_nsec > 1000000000) {
-        ts.tv_sec += 1;
-        ts.tv_nsec -= 1000000000;
-    }
-    if (sem_timedwait(&c->sig, &ts) == -1) {
-        return RAGE_ERROR("Timed out waiting");
-    }
-    return RAGE_OK;
 }
 
 int rage_countdown_add(rage_Countdown * c, int delta) {
     int val = atomic_fetch_add(&c->counter, delta) + delta;
     if (val == 0) {
-        sem_post(&c->sig);
+        c->action(c->args);
     }
     return val;
 }
 
-int rage_countdown_unblock_wait(rage_Countdown * c) {
+int rage_countdown_force_action(rage_Countdown * c) {
     int val = atomic_exchange(&c->counter, 0);
     if (val != 0) {
-        sem_post(&c->sig);
+        c->action(c->args);
     }
     return val;
 }
