6f3fc5d43eb85d5c009a32ebe6cc426d5a1d9e4e
David Honour <david@concertdaw.co.uk>
2019-04-04 11:00:59 +0100

    Test that the bulk backend definitely calls process...
    it did.

--- graph/test/test_bulk_backend.c
+++ graph/test/test_bulk_backend.c
@@ -1,11 +1,19 @@
+#include <semaphore.h>
 #include <bulk_backend.h>
 
-rage_Error test_bulk_backend() {
+static void post_sem_proc(uint32_t n_frames, void * data) {
+    sem_post(data);
+}
+
+static rage_Error test_bulk_backend() {
     rage_Error err = RAGE_OK;
     rage_BulkBackend * b = rage_bulk_backend_new(11, 12);
     rage_BackendInterface * i = rage_bulk_backend_get_interface(b);
     uint32_t ps;
-    rage_BackendHooks h = rage_backend_setup_process(i, NULL, noop, noop, &ps);
+    sem_t proc_sem;
+    sem_init(&proc_sem, 0, 0);
+    rage_BackendHooks h = rage_backend_setup_process(
+        i, &proc_sem, post_sem_proc, noop, &ps);
     if (ps != 12) {
         err = RAGE_ERROR("Did not set period size correctly");
         goto cleanup;
@@ -14,6 +22,7 @@ rage_Error test_bulk_backend() {
     if (RAGE_FAILED(err)) {
         goto cleanup;
     }
+    sem_wait(&proc_sem);
     rage_TickForcing * tf = h.tick_force_start(h.b);
     h.tick_force_end(tf);
     h.get_buffers(h.b, 0, 312, NULL, NULL);
