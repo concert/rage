07ab31f079c35bbf91d1ea4cf4a1f5d22948b289
David Honour <david@concertdaw.co.uk>
2019-05-17 15:42:17 +0100

    Refactor test to make it less nesty-brackety

--- langext/test/test_synchronisation_primitives.c
+++ langext/test/test_synchronisation_primitives.c
@@ -5,27 +5,25 @@ static void incr(void * v) {
     (*((int *) v))++;
 }
 
+static rage_Error countdown_checks(rage_Countdown * c, int * i) {
+    if (*i != 0) {
+        return RAGE_ERROR("Action ran before trigger");
+    }
+    rage_countdown_add(c, -1);
+    if (*i != 0) {
+        return RAGE_ERROR("Triggered early");
+    }
+    rage_countdown_add(c, -1);
+    if (*i != 1) {
+        return RAGE_ERROR("Did not trigger action");
+    }
+    return RAGE_OK;
+}
+
 rage_Error test_countdown() {
     int i = 0;
     rage_Countdown * c = rage_countdown_new(2, incr, (void *) &i);
-    char * rval = NULL;
-    if (i != 0) {
-        rval = "Action ran before trigger";
-    } else {
-        rage_countdown_add(c, -1);
-        if (i != 0) {
-            rval = "Triggered early";
-        } else {
-            rage_countdown_add(c, -1);
-            if (i != 1) {
-                rval = "Did not trigger action";
-            }
-        }
-    }
+    rage_Error rval = countdown_checks(c, &i);
     rage_countdown_free(c);
-    if (rval != NULL) {
-        return RAGE_ERROR(rval);
-    } else {
-        return RAGE_OK;
-    }
+    return rval;
 }
